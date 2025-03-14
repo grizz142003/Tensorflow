f4f909c847984ce6d364d6f6e385704dcb116735
GianlucaRizzo <121537559+grizz142003@users.noreply.github.com>
2025-03-14 12:17:35 -0400

    Add files via upload

---
+++ Venv.sh
@@ -0,0 +1,23 @@
+mkdir PyVenv 
+cd PyVenv
+
+
+python3 -m venv tf
+source tf/bin/activate
+pip install --upgrade pip
+
+if [[$(lshw -C display | grep vendor) =~ Nvidia ]]; then
+    echo 'please install tensorflow with cuda by following directions online'
+else
+    pip install tensorflow
+fi
+
+pip install matplotlib
+pip install tensorflow-hub
+pip install tensorflow-datasets
+pip install seaborn
+pip install git+https://github.com/tensorflow/docs
+pip install pyyaml h5py
+pip install -U keras-tuner
+
+
