fb13f1fcd20179a5d99cea56883ab3f03da9c7cd
GianlucaRizzo <121537559+grizz142003@users.noreply.github.com>
2025-03-17 10:02:26 -0400

    Update VenvGPU.sh

--- VenvGPU.sh
+++ VenvGPU.sh
@@ -8,12 +8,6 @@ python3 -m venv tf
 source tf/bin/activate
 pip install --upgrade pip
 
-#if [[$(lshw -C display | grep vendor) =~ Nvidia ]]; then
-#    echo 'Your computer has a GPU installed please use this webpage to install #tensorflow to work with your GPU'
-#    xdg-open https://www.tensorflow.org/install/pip
-#else
-#    pip install tensorflow
-#fi
 pip install tensorflow[and-cuda]
 pushd $(dirname $(python -c 'print(__import__("tensorflow").__file__)'))
 ln -svf ../nvidia/*/lib/*.so* .
